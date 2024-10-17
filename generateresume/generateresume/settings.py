82dc7e22fd1cda714b4378b0a094550144a294f6
Matheus Alves <matheusalvessilvabr@gmail.com>
2024-10-17 16:40:39 -0300

    api key

--- generateresume/generateresume/settings.py
+++ generateresume/generateresume/settings.py
@@ -13,6 +13,7 @@ import dj_database_url
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+YOUR_API_KEY = config('YOUR_API_KEY')
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5asnvsogcd7@2k@05habb3d-z4(@*9v&b#dxv5dvpu$95++h$h'
+SECRET_KEY = 'django_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
